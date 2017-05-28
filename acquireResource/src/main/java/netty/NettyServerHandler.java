9e9b758811df8753f60ab205c198fa670b680f00
fightingwkk <593658166@qq.com>
2017-05-28 10:23:17 +0800

    ceshi

--- acquireResource/src/main/java/netty/NettyServerHandler.java
+++ acquireResource/src/main/java/netty/NettyServerHandler.java
@@ -32,6 +32,7 @@ public class NettyServerHandler extends ChannelHandlerAdapter {
 		if(body.startsWith("[{\"Interface\":")){
 			JSONArray json =  JSONArray.fromObject(body);
 		}
+		System.out.println();
 		// System.out.println("The time server receive order:" + body
 		// +"; the counter is : "+ ++counter);
 		// String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
